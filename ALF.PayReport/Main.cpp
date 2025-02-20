7182095d5749f4ff62cb3544bada5487a89cf137
Alexander Freimuth <500211023@fvtc.edu>
2025-02-20 14:21:06 -0600

    Add project files.

---
+++ ALF.PayReport/Main.cpp
@@ -0,0 +1,76 @@
+
+// Pay Report
+// Alex Freimuth
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+struct Employee
+{
+    int ID;
+    string FirstName;
+    string LastName;
+    float HoursWorked;
+    float HourlyRate;
+};
+
+int GetNumOfEmployees() 
+{
+    
+    cout << "How many employees do you want to process? ";
+    int size = 0;
+    cin >> size;
+    return size;
+
+}
+
+int main()
+{
+    int size = GetNumOfEmployees();
+
+    int* number = new int[size];
+
+    Employee* employee = new Employee[size];
+
+
+    for (int i = 0; i < size; i++)
+    {
+
+        cout << i << "\n";
+
+        int id;
+        string fname;
+        string lname;
+        float hoursworked;
+        float rate;
+
+        cout << "Insert an ID: ";
+        cin >> id;
+        cout << "Insert a first name: ";
+        cin >> fname;
+        cout << "Insert a last name: ";
+        cin >> lname;
+        cout << "Insert the amount of hours worked: ";
+        cin >> hoursworked;
+        cout << "Insert their hourly rate: ";
+        cin >> rate;
+
+        employee[i] = {id, fname, lname, hoursworked, rate};
+    }
+
+    int totalPay = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        float weeklyPay = employee[i].HoursWorked * employee[i].HourlyRate;
+        cout << weeklyPay << "\n";
+        totalPay = totalPay + weeklyPay;
+        
+    }
+    cout << totalPay;
+
+	(void)_getch();
+	return 0;
+}
